7713645767ec25a666e9408ef5c7c068c3119c47
Ryan Wilson <rycwilson@gmail.com>
2022-10-28 14:42:37 -0700

    adds specific errors to error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,25 @@
-import { ApiError } from '../errors/index.js'
 import { StatusCodes } from 'http-status-codes'
 
 const errorHandler = (err, req, res, next) => {
-  return err instanceof ApiError ?
-    res.status(err.statusCode).json({ msg: err.message }) :
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  const customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Sorry, there was an error'
+  }
+  if (err.name === 'ValidationError') {
+    console.log(Object.values(err.errors).map(item => item.message).join(','))
+    customError.msg = Object.values(err.errors).map(item => item.message).join(', ')
+    customError.statusCode = 400
+  }
+  if (err.name === 'CastError') {
+    customError.msg = `No job found with id: ${err.value}`
+    customError.statusCode = 404
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `That ${Object.keys(err.keyValue)} already exists`
+    customError.statusCode = 400
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 export default errorHandler
