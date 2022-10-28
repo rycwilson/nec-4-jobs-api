479a9faabb75dd8f9883387874845a60ff1862fd
Ryan Wilson <rycwilson@gmail.com>
2022-10-28 11:31:12 -0700

    adds jobs controller methods

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes'
 import Job from '../models/job.js'
-import { NotFoundError, UnauthenticatedError } from '../errors/index.js'
+import { BadRequestError, NotFoundError, UnauthenticatedError } from '../errors/index.js'
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
@@ -16,35 +16,29 @@ const createJob = async (req, res) => {
 const getJob = async (req, res) => {
   const { user: { userId }, params: { id: jobId }} = req
   const job = await Job.findOne({ _id: jobId, createdBy: userId }).select('-createdAt -updatedAt') 
-  if (!job) {
-    throw new NotFoundError(`No job with id: ${jobId}`, 404)
-  // } else if (userId !== job.createdBy.toString()) {
-  //   throw new UnauthenticatedError('Not authorized to access this data')
-  } else {
-    res.status(StatusCodes.OK).json({ job })
-  }
+  if (!job) throw new NotFoundError(`No job with id: ${jobId}`, 404)
+  res.status(StatusCodes.OK).json({ job })
 }
 
 const updateJob = async (req, res) => {
-  const { id: jobId } = req.params
+  const { user: { userId }, body: { company, position }, params: { id: jobId } } = req
+  if (!company || !position) throw new BadRequestError('Missing required fields')
 
   // add option overwrite: true if a PUT with missing attributes should remove or reset those attributes
-  const job = await job.findOneAndUpdate(
-    { _id: jobId }, req.body, { new: true, runValidators: true }
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId }, 
+    req.body, 
+    { new: true, runValidators: true }
   )
-  if (!job) {
-    throw new ApiError(`No job with id: ${jobId}`, 404)
-  }
+  if (!job) throw new NotFoundError(`No job with id: ${jobId}`, 404)
   res.status(200).json({ job })
 }
 
 const deleteJob = async (req, res) => {
-  const { id: jobId } = req.params
-  const job = await job.findOneAndDelete({ _id: jobId })
-  if (!job) {
-    return next(new ApiError(`No job with id: ${jobId}`, 404))
-  }
-  res.status(200).json({ status: 'success' })
+  const { user: { userId }, params: { id: jobId }} = req
+  const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId })
+  if (!job) throw new ApiError(`No job with id: ${jobId}`, 404)
+  res.status(200).send()
 }
 
 export { getAllJobs, createJob, getJob, updateJob, deleteJob }
